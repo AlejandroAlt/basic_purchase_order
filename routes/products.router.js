2cccba372afec540d4e3eb9ad24a1b10496b92f8
AlejandroAlt <alejandro.altamirano.elias@gmail.com>
2021-10-17 23:58:05 -0600

    feat: services
    add products services

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,14 @@
 const express = require ('express');
 
+const ProductsService = require('./../services/products.services')
+
 const router = express.Router();
+const service = new ProductsService();
+
+router.get('/', (req,res) => {
+    const products = service.find();
+    res.json(products);
+});
 
 router.get('/filter', (req, res) => {
     res.send('I am a filter');
@@ -8,37 +16,27 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req,res) => {
     const {id} = req.params;
-    res.json({
-        id,
-        name: 'prt01',
-        price: 100
-    })
+    const product = service.findOne(id);
+    res.json(product);
 });
 
 router.post('/', (req, res) => {
     const body = req.body;
-    res.json({
-        message: 'created',
-        data: body
-    })
+    const newProduct = service.create(body);
+    res.status(201).json(newProduct);
 });
 
 router.patch('/:id', (req, res) => {
     const {id} = req.params;
     const body = req.body;
-    res.json({
-        message: 'updated',
-        data: body,
-        id
-    })
+    const product = service.update(id, body);
+    res.json(product);
 });
 
 router.delete('/:id', (req, res) => {
     const {id} = req.params;
-    res.json({
-        message: 'deleted',
-        id
-    })
+    const rta = service.delete(id);
+    res.json(rta);
 });
 
 module.exports = router;
