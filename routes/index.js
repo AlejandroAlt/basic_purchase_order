9621f2e9b70ba2823acefa70cf977d94d306bd7e
AlejandroAlt <alejandro.altamirano.elias@gmail.com>
2021-10-20 22:01:13 -0600

    feat: routes
    Add routes for orders, orderstatus, roles and users

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const productsRouter = require('./products.router');
+const usersRouter = require('./users.router');
+const ordersRouter = require('./orders.router');
+const rolesRouter = require('./roles.router');
+const orderstatusRouter = require('./orderstatus.router');
 
 function routerApi(app)
 {
     app.use('/products', productsRouter);
+    app.use('/users', usersRouter);
+    app.use('/orders', ordersRouter);
+    app.use('/roles', rolesRouter);
+    app.use('/orderstatus', orderstatusRouter);
 }
 
 module.exports = routerApi;
---
+++ routes/users.router.js
@@ -0,0 +1,44 @@
+const express = require ('express');
+
+const router = express.Router();
+
+router.get('/filter', (req, res) => {
+    res.send('I am a filter');
+});
+
+router.get('/:id', (req,res) => {
+    const {id} = req.params;
+    res.json({
+        id,
+        name: 'usr01',
+        price: 100
+    })
+});
+
+router.post('/', (req, res) => {
+    const body = req.body;
+    res.json({
+        message: 'created',
+        data: body
+    })
+});
+
+router.patch('/:id', (req, res) => {
+    const {id} = req.params;
+    const body = req.body;
+    res.json({
+        message: 'updated',
+        data: body,
+        id
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    res.json({
+        message: 'deleted',
+        id
+    })
+});
+
+module.exports = router; 
